bcbe1c5dceb244c74d92b1eefb15d0a13946cdb2
fengpan420 <fengpan420@qq.com>
2020-08-16 18:09:03 +0800

    init commit

--- micro-service-consumer/src/main/java/org/fengpp/springcloud/service/UserCilent.java
+++ micro-service-consumer/src/main/java/org/fengpp/springcloud/service/UserCilent.java
@@ -2,12 +2,23 @@ package org.fengpp.springcloud.service;
 
 import org.fengpp.springcloud.vo.User;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(value = "MICRO-SERVICE-PROVIDER")
+@FeignClient(value = "MICRO-SERVICE-PROVIDER", fallback = HystrixClientFallback.class)
 public interface UserCilent {
     @RequestMapping("/provider/user/{id}")
         //user/1
     User getUser(@PathVariable("id") Long id);
+}
+
+@Component
+class HystrixClientFallback implements UserCilent {
+    @Override
+    public User getUser(Long id) {
+        System.out.println("熔断，默认回调函数");
+        User u = new User(1L, "admin");
+        return u;
+    }
 }
